c31f513e1f73481d084154ba1fd5252588b2b470
GabeFansh <94994116+GabeFansh@users.noreply.github.com>
2023-09-06 09:24:57 -0400

    hi

--- functions/scrollText.js
+++ functions/scrollText.js
@@ -1,21 +1,24 @@
-var greetings = [ "Gabe figured out scrolling text!", 
-"232 is very boring", 
-"I'm not ready for finals :(", 
-"Coding is fun (help)", 
-"Hopefully this website looks decent", 
-"I've run out of ideas for this", 
-"Wait i figured out a new idea"
+var greetings = [ "text1", 
+"text2", 
+"text3", 
+"text4", 
+"text5", 
+"text6", 
+"text7", 
+"text8", 
+"text9", 
+"text10"
 ];
 
-var seconds = 10;
+var msecs = 4000; //Milliseconds that will wait to change the text
+var marq = document.getElementById("marq"); //The object
+var pix = marq.clientWidth; //The width in the screen
 
-var time = seconds * 1000;
+//You have to vary this... If you know a little bit of maths they can help you.
+marq.scrollAmount = (pix / (msecs / 50));
 
-//Maybe this is clearer for you...
-setInterval(changeText, time);
-
-function changeText() 
+setInterval(function() 
 {
     var greeting_id = Math.floor(Math.random() * greetings.length);
-    document.getElementById('breakingNews').innerHTML = "Breaking News: " + greetings[greeting_id];
-}
+    document.getElementById('breakingNews').innerHTML = greetings[greeting_id];
+}, msecs);
